16b10ab272e0974b27824605fb1ab735ee49fd94
Augusto Dias <augustohdiasz@gmail.com>
2025-12-05 00:53:58 -0300

    feat:maybe first playable version

--- TDS_inca_cosmogony/install.sh
+++ TDS_inca_cosmogony/install.sh
@@ -4,12 +4,18 @@ MOD_DIR="TDS_inca_cosmogony"
 
 cd ./objects
 rm -f *.txt
+echo "Building templates..."
 BuildDFT ./templates
 cd ..
-rm -rf "$HOME/.local/share/Steam/steamapps/common/Dwarf Fortress/mods/$MOD_DIR"
+echo "Cleaning up old version files..."
+rm -rf "$HOME/.local/share/Steam/steam/apps/common/Dwarf Fortress/mods/$MOD_DIR"
 mkdir -p "$HOME/.local/share/Steam/steamapps/common/Dwarf Fortress/mods/$MOD_DIR/objects"
 mkdir -p "$HOME/.local/share/Steam/steamapps/common/Dwarf Fortress/mods/$MOD_DIR/graphics"
+echo "Copying new files..."
 cp -r ./objects/* "$HOME/.local/share/Steam/steamapps/common/Dwarf Fortress/mods/$MOD_DIR/objects"
 cp -r ./graphics/* "$HOME/.local/share/Steam/steamapps/common/Dwarf Fortress/mods/$MOD_DIR/graphics"
 cp ./info.txt "$HOME/.local/share/Steam/steamapps/common/Dwarf Fortress/mods/$MOD_DIR/"
+echo "Removing old installation..."
 rm -rf "$HOME/.local/share/Steam/steamapps/common/Dwarf Fortress/data/installed_mods/$MOD_DIR"*
+rm -rf "$HOME/.local/share/Bay 12 Games/Dwarf Fortress/data/installed_mods/$MOD_DIR"*
+echo "done."
